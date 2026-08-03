2c0de24ca36c496ed8c165cd9d150ff89efbcf71
Kush Gupta <kushguptacse@gmail.com>
2026-08-03 06:33:49 +0000

    feat: add ticket pricing tool and implement tool calling support in chatbot agent

--- aireline_chatbot_agent.py
+++ aireline_chatbot_agent.py
@@ -8,17 +8,55 @@ Give short, courteous answers, no more than 1 sentence.
 Always be accurate. If you don't know the answer, just say I don't know. """
 messages.append({"role": "system", "content": f"{system_prompt}"})
 
-ticket_dict = {"new york": 500, "london": 600, "paris": 700}
+ticket_dict = {"new york": 500, "london": 600, "paris": 700, "dubai": 650, "delhi": 800, "tokyo": 900, "sydney": 1000}
 
 def get_ticket_price(destination):
     return ticket_dict.get(destination.lower(), f"{destination} destination not yet supported!!!!")
 
+get_ticket_price_function = {
+    "name": "get_ticket_price",
+    "description": "Get the price of a return ticket to the destination city.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "destination_city": {
+                "type": "string",
+                "description": "The city that the customer wants to travel to",
+            },
+        },
+        "required": ["destination_city"],
+        "additionalProperties": False
+    }
+}
+
+tools = [{"type": "function", "function": get_ticket_price_function}]
+
 def chat_response(message, history):
     history = [{"role":h["role"], "content":h["content"]} for h in history]
     messages.append({"role": "user", "content": f"{message}"})
-    response = call_chat_api(messages)
+    response = call_chat_api(messages, tools)
+    if response.choices[0].finish_reason=="tool_calls":
+        response  = handle_tool_call(response.choices[0].message)
+        messages.append(response.choices[0].message)
+        messages.append(response)
+        response = call_chat_api(messages)
     messages.append({"role": "assistant", "content": f"{response.choices[0].message.content}"})
     return response.choices[0].message.content
+
+def handle_tool_call(message):
+    print(f"handle tool call for message {message}")
+    tool_call = message.tool_calls[0]
+    if tool_call.function.name == "get_ticket_price":
+        arguments = json.loads(tool_call.function.arguments)
+        city = arguments.get('destination_city')
+        price_details = get_ticket_price(city)
+        response = {
+            "role": "tool",
+            "content": price_details,
+            "tool_call_id": tool_call.id
+        }
+    return response
+    
 
 chat = gr.ChatInterface(fn=chat_response, title="FlyAirline Chatbot Agent")
 chat.launch()
